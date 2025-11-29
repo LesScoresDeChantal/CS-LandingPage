b54f301b977a2ab08e4040d87072376befd6c787
JeremyLP <jeremyleprunenec.pro@gmail.com>
2025-11-29 13:35:04 +0100

    chore: Update nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com https://t.contentsquare.net",
+              "connect-src 'self' https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com",
+              "img-src 'self' data: https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com",
+            ]
+              .join("; ")
+              .replace(/\s+/g, " "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
